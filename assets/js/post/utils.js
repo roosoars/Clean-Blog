eeab59d1a7ed5d506955abede4a61430f919b221
Rodrigo <roosoars@icloud.com>
2024-06-13 15:36:14 -0300

    Delete assets directory

--- assets/js/post/utils.js
+++
@@ -1,4 +0,0 @@
-export function formatDate(date) {
-    const options = { day: '2-digit', month: 'short', year: 'numeric' };
-    return date.toLocaleDateString('pt-BR', options).toUpperCase().replace(/\.| DE/g, '');
-}
